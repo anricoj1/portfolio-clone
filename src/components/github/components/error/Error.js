0bfde8f735aeba123db0a1afa84969b22add550e
= <=>
2020-11-18 20:29:33 -0500

    flexbox collapse

---
+++ src/components/github/components/error/Error.js
@@ -0,0 +1,14 @@
+/* react */
+import React from 'react';
+
+/* css */
+import './Error.css';
+
+
+const Error = ({ msg }) => {
+    return (
+        <div>{msg}</div>
+    )
+}
+
+export default Error;
